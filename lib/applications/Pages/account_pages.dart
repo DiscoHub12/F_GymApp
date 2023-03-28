5531d67eac99ede3660a1d0c1615d4e3ee230d54
DiscoHub12 <discordo12di@gmail.com>
2023-03-28 19:24:21 +0200

    [CONTINUE] Implementation for Account page.

--- lib/applications/Pages/account_pages.dart
+++ lib/applications/Pages/account_pages.dart
@@ -1,71 +1,168 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:gymapp/Other/image_string.dart';
 // ignore: library_prefixes
+import '../../Other/image_string.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import '../Utils/drawer.dart' as Drawer;
 
-class Account extends StatefulWidget {
-  const Account({super.key});
+class AccountPage extends StatefulWidget {
+  const AccountPage({super.key});
 
   @override
-  State<Account> createState() => _MyAccount();
+  State<AccountPage> createState() => _MyAccount();
 }
 
-class _MyAccount extends State<Account> {
+class _MyAccount extends State<AccountPage> {
+  //TextEditingController for Input:
+  final TextEditingController name = TextEditingController();
+  final TextEditingController cognome = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController eta = TextEditingController();
 
-
+  //File for change Photo:
+  late File image;
 
   @override
   Widget build(BuildContext context) {
-    var isDard = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Scaffold(
-      drawer: const Drawer.NavigationDrawer(),
-      appBar: AppBar(
-        title: const Text('Profile'),
-        backgroundColor: Colors.orange,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              SizedBox(
-                width: 120, height: 120,
-                child: ClipRRect( borderRadius: BorderRadius.circular(100), child : Image(image: AssetImage(tProfileImage),),),
-              ), 
-              const SizedBox(height: 20,), 
-              Text('Nome', style: Theme.of(context).textTheme.headline4,),
-              Text('Email',style: Theme.of(context).textTheme.bodySmall,), 
-              const SizedBox(height: 20,), 
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.yellow, side: BorderSide.none, shape: const StadiumBorder(),
-                  ),
-                  child: const Text('Edit Profile', style: TextStyle(color: Colors.black),),
+        drawer: const Drawer.NavigationDrawer(),
+        appBar: AppBar(
+          title: const Text('Account'),
+          backgroundColor: Colors.orange,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: const Image(
+                          image: AssetImage(tProfileImage),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap: () async {
+                          await getImage();
+                          await saveImage();
+                        },
+                        child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.orange,
+                            ),
+                            child: const Icon(
+                              Icons.create_outlined,
+                              size: 20.0,
+                              color: Colors.black,
+                            )),
+                      ),
+                    )
+                  ],
                 ),
-              ), 
-              const SizedBox(height: 20,),
-              const Divider(),
-              const SizedBox(height: 10,),
-              ListTile(
-                leading: Container(
-                  width: 30, height: 30,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100), 
-                    color: Colors.yellow.withOpacity(0.1), 
-                  ),
-                  child: Icon(Icons.settings, color: Colors.black,),
-
+                const SizedBox(
+                  height: 50,
                 ),
-                title: Text('Settings', style: Theme.of(context).textTheme.bodyMedium,),
-              )
-
-            ],
+                Form(
+                  child: Column(
+                    children: [
+                      _formField('Nome', name, Icons.person),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _formField('Cognome', cognome, Icons.person),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _formField('Email', email, Icons.email),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _formField('Eta', email, Icons.date_range),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(onPressed: () async {
+          final picker = ImagePicker();
+          final pickedFile =
+              await picker.pickImage(source: ImageSource.gallery);
+
+          if (pickedFile != null) {
+            final appDocumentsDirectory =
+                await getApplicationDocumentsDirectory();
+            final newFilePath = '${appDocumentsDirectory.path}/image.jpg';
+
+            final newFile = await pickedFile.path == newFilePath;
+          }
+        }));
+  }
+
+  Widget _formField(String text, TextEditingController controller, icons) =>
+      TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: const BorderSide(color: Colors.black),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: const BorderSide(color: Colors.black),
+          ),
+          label: Text(text),
+          labelStyle: const TextStyle(color: Colors.black),
+          prefixIcon: Icon(
+            icons,
+            color: Colors.black,
+          ),
+        ),
+      );
+
+  //Method for this Class :
+  Future getImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/profile.png');
+  }
+
+  Future<void> saveImage() async {
+    final file = await _localFile;
+    await file.writeAsBytes(image.readAsBytesSync());
   }
 }
