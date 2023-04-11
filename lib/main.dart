216a1c4de404e873d03d457798baad0bfee59758
DiscoHub12 <discordo12di@gmail.com>
2023-04-11 22:58:07 +0200

    [ADD] New Boxes into main.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:gymapp/app.dart';
 import 'package:gymapp/applications/Models/account.dart';
 import 'package:gymapp/applications/Models/day_schedule.dart';
 import 'package:gymapp/applications/Models/exercise.dart';
+import 'package:gymapp/applications/Models/favourite_list_workout.dart';
 import 'package:gymapp/applications/Models/workout.dart';
 // ignore: unnecessary_import
 import 'package:hive/hive.dart';
@@ -16,10 +17,12 @@ void main() async {
   Hive.registerAdapter(DayScheduleAdapter());
   Hive.registerAdapter(ExerciseAdapter());
   Hive.registerAdapter(WorkoutAdapter());
+  Hive.registerAdapter(FavouriteWorkoutAdapter());
   //Open the Hive Box :
   await Hive.openBox<Account>('getAccount');
   await Hive.openBox<Exercise>('getExercise');
   await Hive.openBox<DaySchedule>('getDaySchedule');
   await Hive.openBox<Workout>('getWorkout');
+  await Hive.openBox<FavouriteWorkout>('getFavouriteWorkout');
   runApp(const MaterialApp(home: App()));
 }
