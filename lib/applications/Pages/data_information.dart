0f06cc45f3a9b554b691f250f1156fa571315453
DiscoHub12 <discordo12di@gmail.com>
2023-04-13 19:39:39 +0200

    [INIT] Implementation for Data Information.

--- lib/applications/Pages/data_information.dart
+++ lib/applications/Pages/data_information.dart
@@ -10,6 +10,11 @@ class DataInfo extends StatefulWidget {
 }
 
 class _MyDataInfo extends State<DataInfo> {
+  final String username = "Username";
+  final int numSchede = 10;
+  final String lastWorkout = "1";
+  final String lastSchedule = "Schedule";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +24,66 @@ class _MyDataInfo extends State<DataInfo> {
         centerTitle: true,
         backgroundColor: Colors.orange,
       ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[Text("Information Workout")],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 70,
+              backgroundColor: Colors.grey,
+              child: Text(
+                username[0].toUpperCase(),
+                style: TextStyle(fontSize: 60, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Benvenuto, $username!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Statistiche allenamenti",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text("Ultimo allenamento: $lastWorkout"),
+                    Text("Ultima scheda utilizzata: $lastSchedule"),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Statistiche schede",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text("Numero di schede: $numSchede"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
